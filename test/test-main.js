08abdb9de2c4830a09b7be56b9ab7acae26fe453
Rishabh Rao <rishabhsrao@gmail.com>
2013-11-17 21:14:31 +0530

    Created tests for other prefs.

--- test/test-main.js
+++ test/test-main.js
@@ -1,13 +1,25 @@
 var $simplePrefs = require("sdk/simple-prefs");
 
-/**
- * Tests `preferences.website` from `package.json`.
- */
-exports["test Notifyr.jetpack.simplePrefs preferences.website"] = function(assert) {
-  var expected = "mail.accord-soft.com";
-  var actual = $simplePrefs.prefs["preferences.website"];
+exports["test $simplePrefs preferences"] = function(assert) {
+  var expected = {
+    website: "mail.accord-soft.com",
+    path: {
+      mailCounts: "html body table tbody tr td span small font",
+      lastRefresh: "html body table tbody tr td table tbody tr td small span:last-child"
+    }
+  };
 
-  assert.ok(expected === actual);
+  var actual = {
+    website: $simplePrefs.prefs["preferences.website"],
+    path: {
+      mailCounts: $simplePrefs.prefs["preferences.path.mailCounts"],
+      lastRefresh: $simplePrefs.prefs["preferences.path.lastRefresh"]
+    }
+  };
+
+  assert.ok(expected.website === actual.website);
+  assert.ok(expected.path.mailCounts === actual.path.mailCounts);
+  assert.ok(expected.path.lastRefresh === actual.path.lastRefresh);
 };
 
 require("sdk/test").run(exports);
