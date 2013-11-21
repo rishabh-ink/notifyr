2ea9db22c3c5b44efdd5c64a64c99a3b849c40bd
Rishabh Rao <rishabhsrao@gmail.com>
2013-11-21 22:55:24 +0530

    Used JSON.stringify() for logging.

--- data/element-grabber.js
+++ data/element-grabber.js
@@ -8,13 +8,13 @@ self.port.on("notifyr.dom.getElements", function(paths) {
 
     console.warn("notifyr", "Testing elements");
     if(null === elements.mailCounts || null === elements.lastRefresh) {
-      console.warn("notifyr", "One or more elements not found", elements);
+      console.warn("notifyr", "One or more elements not found", JSON.stringify(elements));
     } else {
       console.info("notifyr", "Obtained elements", elements);
-      self.port.emit("notifyr.dom.gotElements", elements);
+      self.port.emit("notifyr.dom.gotElements", JSON.stringify(elements));
     }
   } catch(e) {
-    console.warn("notifyr", "Error obtaining elements", paths);
+    console.warn("notifyr", "Error obtaining elements", JSON.stringify(paths));
     self.port.emit("notifyr.error", e);
   }
 });
