1af7afe11e958d27be4424c29884c6fc237c0e06
Rishabh Rao <rishabhsrao@gmail.com>
2013-11-23 09:09:56 +0530

    Refactored two separate events into one.

--- data/parser.js
+++ data/parser.js
@@ -1,43 +1,46 @@
 function parseMailCounts(rawMailCounts, regexpMailCounts) {
   console.log("notifyr", "main", "parseMailCounts", rawMailCounts);
 
-  try {
-    console.log("notifyr", "main", "parseMailCounts", "Building regexp object using", regexpMailCounts);
-    var re = new RegExp(regexpMailCounts, "g");
+  console.log("notifyr", "main", "parseMailCounts", "Building regexp object using", regexpMailCounts);
+  var re = new RegExp(regexpMailCounts, "g");
 
-    console.log("notifyr", "main", "parseMailCounts", "Parsing using", re.source);
-    var counts = rawMailCounts.match(re);
+  console.log("notifyr", "main", "parseMailCounts", "Parsing using", re.source);
+  var counts = rawMailCounts.match(re);
 
-    console.log("notifyr", "main", "parseMailCounts", "Assigning counts", JSON.stringify(counts));
-    var mailCounts = {
-      unreadMail: parseInt(counts[0], 10),
-      total: parseInt(counts[1], 10)
-    };
+  console.log("notifyr", "main", "parseMailCounts", "Assigning counts", JSON.stringify(counts));
+  var mailCounts = {
+    unreadMail: parseInt(counts[0], 10),
+    total: parseInt(counts[1], 10)
+  };
 
-    console.log("notifyr", "main", "parseMailCounts", "Assigned counts", JSON.stringify(mailCounts));
-    self.port.emit("notifyr.parser.parsedMailCounts", mailCounts);
-  } catch(e) {
-    console.log("notifyr", "main", "parseMailCounts", "Error parsing mail counts", e.message);
-    self.port.emit("notifyr.error", e);
-  }
+  console.info("notifyr", "main", "parseMailCounts", "Assigned counts", JSON.stringify(mailCounts));
+  return mailCounts;
 }
 
 function parseLastRefresh(rawLastRefresh) {
   console.log("notifyr", "main", "parseLastRefresh", rawLastRefresh);
 
-  try {
-    console.log("notifyr", "main", "parseLastRefresh", "Stripping nbsp-s from", rawLastRefresh);
-    rawLastRefresh = rawLastRefresh.replace(/&nbsp;/g, "");
+  console.log("notifyr", "main", "parseLastRefresh", "Stripping nbsp-s from", rawLastRefresh);
+  rawLastRefresh = rawLastRefresh.replace(/&nbsp;/g, "");
 
-    console.log("notifyr", "main", "parseLastRefresh", "Creating date from", rawLastRefresh);
-    var lastRefresh = Date.parse(rawLastRefresh);
+  console.log("notifyr", "main", "parseLastRefresh", "Creating date from", rawLastRefresh);
+  var lastRefresh = Date.parse(rawLastRefresh);
 
-    self.port.emit("notifyr.parser.parsedLastRefresh", lastRefresh);
-  } catch(e) {
-    console.log("notifyr", "main", "parseLastRefresh", "Error parsing last refresh", e.message);
-    self.port.emit("notifyr.error", e);
-  }
+  console.info("notifyr", "main", "parseLastRefresh", "Created date", lastRefresh);
+  return lastRefresh;
 }
 
-self.port.on("notifyr.parser.parseMailCounts", parseMailCounts);
-self.port.on("notifyr.parser.parseLastRefresh", parseLastRefresh);
+self.port.on("notifyr.parser.parse", function(toParse) {
+  try {
+    var mailCounts = parseMailCounts(toParse.mailCounts.rawData, toParse.mailCounts.regexp);
+    var lastRefresh = parseLastRefresh(toParse.lastRefresh.rawData);
+
+    self.port.emit("notifyr.parser.parsed", {
+      mailCounts: mailCounts,
+      lastRefresh: lastRefresh
+    });
+  } catch(e) {
+    console.error("notifyr", "main", "parseLastRefresh", "Error parsing last refresh", e.message);
+    self.port.emit("notifyr.error", e);
+  }
+});
